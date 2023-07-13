a6b70fa5bc37ddb4d4c3480e5de97a503f5ceeb5
Patrycja Dziabala <patrycja.dziabala@gmail.com>
2023-07-13 02:00:36 +0200

    Add Settings View

---
+++ BudgetLand/Views/SettingsView/ManageCategoriesView.swift
@@ -0,0 +1,20 @@
+//
+//  ManageCategoriesView.swift
+//  BudgetLand
+//
+//  Created by Patka on 13/07/2023.
+//
+
+import SwiftUI
+
+struct ManageCategoriesView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ManageCategoriesView_Previews: PreviewProvider {
+    static var previews: some View {
+        ManageCategoriesView()
+    }
+}
